b216a7f15e3b5804cbba920e0da403bd4d765b6e
lqewo <vrkonosha@mail.ru>
2021-07-14 16:13:53 +0300

    Create popup

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 var a = document.getElementById('header__item-btn');
+var b = document.getElementById('footer__text-link');
+var c = document.getElementById('popup__closed');
 
 a.onclick = function btn_click () {
     console.log("Button clicked")
     return
 }
+
+b.onclick = function hidden__scroll() {
+    document.body.style.overflowX = 'hidden';
+    document.body.style.overflowY = 'hidden';
+}
+
+c.onclick = function show__scroll() {
+    document.body.style.overflowY = 'visible';
+    document.body.style.overflowX = 'auto';
+}
+
+$('.popup__scroll').mCustomScrollbar({
+    theme: 'default',
+});
